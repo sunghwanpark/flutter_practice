e724133f4f777a323f3a58195d6bdf0447f0a13e
ParkSungHwan <deicide17@gmail.com>
2019-03-05 13:31:24 +0900

    move private url

--- lib/Data/Address.dart
+++ lib/Data/Address.dart
@@ -1,7 +1,3 @@
-const String noticeURL = "https://apply.lh.or.kr";
-const String apiURL = "/RCMC_LCC_SIL_SILSNOT_L0001.jsp";
-const String api_key = "SIELqdq7WDfAldOvf352OQ";
-
 enum Notice_Code
 {
   land,
